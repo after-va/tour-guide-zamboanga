d51f362fca19480e8926f0af946fd6964a71e084
unknown <alelybitun.school@gmail.com>
2025-10-09 10:39:06 +0800

    feat: Add Add and Delete function in phone-number.php

---
+++ classes/phone-number.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once "database.php";
+
+class Phone_Number extends Database {
+    public $phone_ID = "";
+    public $countrycode_ID = "";
+    public $phone_number = "";
+
+    public function addPhoneNumber(){
+        $sql = "INSERT INTO phone_number(countrycode_id, phone_number) VALUES (:countrycode_id, :phone_number)";
+        $query = $this->connect()->prepare($sql);
+        $query->bindParam(":countrycode_id", $this->countrycode_id);
+        $query->bindParam(":phone_number", $this->phone_number);
+
+        return $query->execute();
+    }
+
+    public function deletePhoneNumber($pid){
+        $sql = "DELETE FROM phone_number WHERE phone_id = :id";
+        $query = $this->connect()->prepare($sql);
+        $query->bindParam(":id", $pid);
+
+        return $query->execute();
+
+    }
+
+
+
+
+
+
+}
