0cc52b9db944d4c0ff008eedb02c70c3198c2f80
unknown <alelybitun.school@gmail.com>
2025-10-11 20:25:32 +0800

    feat: Add add-name-info.php

---
+++ experimental-folders/add-name-info.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once "../classes/name-info.php";
+
+$nameinfoObj = new Name_Info ();
+
+$name = [];
+$errors = [];
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+
+
+
+
+}
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Name</title>
+</head>
+<body>
+    <h1>Add Name Info</h1>
+    <form action="" method="post">
+
+    </form>
+</body>
+</html>
