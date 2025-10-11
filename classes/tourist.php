e93762ac3c58bfcb24b4d84831e59ff4165f5365
unknown <alelybitun.school@gmail.com>
2025-10-11 21:57:46 +0800

    feat: addgetPhoneNumber(),addgetEmergencyID(),addgetContactInfo() in tourist.php

--- classes/tourist.php
+++ classes/tourist.php
@@ -106,10 +106,97 @@ class Tourists extends Database{
             return false;
         }
 
-    }    
+    }
+    
+    
+    //Phone_ID
+    public function addgetPhoneNumber($countrycode_ID,$phone_number){
+        $sql = "SELECT phone_ID FROM phone_number WHERE phone_number = :phone_number AND countrycode_ID = :countrycode_ID ";
+        $query = $this->connect()->prepare($sql);
+        $query->bindParam(":countrycode_ID", $countrycode_ID);
+        $query->bindParam(":phone_number", $phone_number);
+        $query->execute();
+        $result = $query->fetch();
+
+        if($result){
+            return $result["phone_ID"];
+        }
+        $db = $this->connect();
+        $sql = "INSERT INTO phone_number(countrycode_ID, phone_number) VALUES (:countrycode_ID, :phone_number)";
+        $query = $db->prepare($sql);
+        $query->bindParam(":countrycode_ID", $countrycode_ID);
+        $query->bindParam(":phone_number", $phone_number);
+
+        if ($query->execute()) {
+            return $db->lastInsertId(); 
+        } else {
+            return false;
+        }
+    }
+
+    // Emergency_ID
+    public function addgetEmergencyID($countrycode_ID, $phone_number, $ename, $erelationship){
+
+        $db = $this->connect(); 
+
+        try {
+             $phone_ID = $this->addgetPhoneNumber($countrycode_ID, $phone_number);
+
+            if(!$phone_ID){
+                $db->rollBack(); 
+                return false;
+            }
+            
+            $sql = "INSERT INTO Emergency_Info (phone_ID, emergency_Name, emergency_Relationship) VALUES (:phone_ID, :ename, :erelationship)";
+            $query = $db->prepare($sql);
+            $query->bindParam(":phone_ID", $phone_ID);
+            $query->bindParam(":ename", $ename); 
+            $query->bindParam(":erelationship", $erelationship); 
+
+            if ($query->execute()){
+                return $db->lastInsertId();
+            } else {
+                return false;
+            }
+
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    // Contact Info
+    public function addContact_Info($houseno, $street, $barangay, $city, $province, $country, $countrycode_ID,$phone_number, $emergency_name, $emergency_countrycode_ID, $emergency_phonenumber, $emergency_relationship, $contactinfo_email){
+        
+        $db = $this->connect();
+
+        try{
+            
+            $address_ID = $this->addgetAddress($houseno, $street, $barangay, $city, $province, $country);
+            $phone_ID = $this->addgetPhoneNumber($countrycode_ID,$phone_number);
+            $emergency_ID = $this->addgetEmergencyID($emergency_countrycode_ID, $emergency_phonenumber, $emergency_name, $emergency_relationship);
+            
+           if (!$address_ID || !$phone_ID || !$emergency_ID) {
+                return false;
+            }
+
+            $sql = "INSERT INTO Contact_Info (address_ID,phone_ID,emergency_ID, contactinfo_email) VALUES (:address_ID, :phone_ID, :emergency_ID, :contactinfo_email)";
+            $query = $db->prepare($sql);
+            $query->bindParam(":address_ID", $address_ID);
+            $query->bindParam(":phone_ID", $phone_ID);
+            $query->bindParam(":emergency_ID", $emergency_ID);
+            $query->bindParam(":contactinfo_email", $contactinfo_email);
+
+            if ($query->execute()){
+                return $db->lastInsertId();
+            } else {
+                return false;
+            }
 
 
-
+        }catch (PDOException $e) {
+            return false;
+        }
+    }
 
 
 
