4bfedf2a7425a642e793869602aa390781a06d93
unknown <alelybitun.school@gmail.com>
2025-10-11 20:43:46 +0800

    update: Update add-name-info.php: Succesfully Can Add in Database

--- experimental-folders/add-name-info.php
+++ experimental-folders/add-name-info.php
@@ -8,14 +8,45 @@ $name = [];
 $errors = [];
 
 
+    // name_first 
+    // name_second 
+    // name_middle
+    // name_last
+    // name_suffix 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name["name_first"] = trim(htmlspecialchars($_POST["name_first"]));
+    $name["name_second"] = trim(htmlspecialchars($_POST["name_second"]));
+    $name["name_middle"] = trim(htmlspecialchars($_POST["name_middle"]));
+    $name["name_last"] = trim(htmlspecialchars($_POST["name_last"]));
+    $name["name_suffix"] = trim(htmlspecialchars($_POST["name_suffix"]));
 
+    if(empty($name["name_first"] )){
+        $errors["name_first"] = "First Name is required first";
+    }
+   
+    if(empty($name["name_last"] )){
+        $errors["name_last"] = "Last Name is required first";
+    }
+    
+    if(empty(array_filter($errors))){
+        $nameinfoObj->name_first = $name["name_first"];
+        $nameinfoObj->name_second = $name["name_second"];
+        $nameinfoObj->name_middle = $name["name_middle"];
+        $nameinfoObj->name_last = $name["name_last"];
+        $nameinfoObj->name_suffix =$name["name_suffix"];
 
+        if($nameinfoObj->addNameInfo()){
+            header("Location: add-name-info.php");
+            exit;
+        } else {
+            echo failed;
+        }
+
+    }
 
 
 
 }
-
 
 
 ?>
@@ -31,7 +62,22 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <body>
     <h1>Add Name Info</h1>
     <form action="" method="post">
+        <label for="name_first">First Name</label>
+        <input type="text" name="name_first" id="name_first" value= "<?= $name["name_first"] ?? '' ?>">
 
+        <label for="name_second"> Second Name </label>
+        <input type="text" name="name_second" id="name_second" value= "<?=$name["name_second"] ?? "" ?>">
+
+        <label for="name_middle">Middle Name</label>
+        <input type="text" name="name_middle" id="name_middle" value= "<?=$name["name_middle"] ?? '' ?>">
+
+        <label for="name_last">Last Name</label>
+        <input type="text" name="name_last" id="name_last" value = "<?= $name["name_last"] ?? '' ?>">
+
+        <label for="name_suffix">Suffix</label>
+        <input type="text" name="name_suffix" id="name_suffix" value = "<?= $name["name_suffix"] ?? '' ?>">
+
+        <input type="submit" value="Save Name">
     </form>
 </body>
 </html>
