1b1dae430f1c594de6b17a3a21d1eef39334e09b
unknown <alelybitun.school@gmail.com>
2025-10-25 21:27:47 +0800

    update: move the other classes in the consuming-class folder and add trait: contact-info

---
+++ classes/trait/trait-contact-info.php
@@ -0,0 +1,66 @@
+<?php
+
+trait ContactInfoTrait {
+
+    public function addContact_Info(
+        $houseno, 
+        $street, 
+        $barangay, 
+        $city, 
+        $province, 
+        $country, 
+        $countrycode_ID,
+        $phone_number, 
+        $emergency_name, 
+        $emergency_countrycode_ID, 
+        $emergency_phonenumber, 
+        $emergency_relationship, 
+        $contactinfo_email
+    ){
+
+        $db = $this->connect();
+        $db->beginTransaction();
+
+        try{
+            // These functions come from other traits:
+            $address_ID = $this->addgetAddress($houseno, $street, $barangay, $city, $province, $country);
+            $phone_ID = $this->addgetPhoneNumber($countrycode_ID, $phone_number);
+            $emergency_ID = $this->addgetEmergencyID($emergency_countrycode_ID, $emergency_phonenumber, $emergency_name, $emergency_relationship);
+            
+           if (!$address_ID || !$phone_ID || !$emergency_ID) {
+                $db->rollBack();
+                return false;
+            }
+
+            $sql = "INSERT INTO Contact_Info (address_ID, phone_ID, emergency_ID, contactinfo_email) 
+                    VALUES (:address_ID, :phone_ID, :emergency_ID, :contactinfo_email)";
+            $query = $db->prepare($sql);
+            $query->bindParam(":address_ID", $address_ID);
+            $query->bindParam(":phone_ID", $phone_ID);
+            $query->bindParam(":emergency_ID", $emergency_ID);
+            $query->bindParam(":contactinfo_email", $contactinfo_email);
+
+            if ($query->execute()){
+                $db->commit();
+                return true; 
+            } else {
+                $db->rollBack();
+                return false;
+            }
+
+        } catch (PDOException $e) {
+            $db->rollBack();
+            return false;
+        }
+    }
+
+
+    public function deleteContactInfo($contact_ID){
+        $sql = "DELETE FROM contact_info WHERE contact_ID = :id";
+        $query = $this->connect()->prepare($sql);
+        $query->bindParam(":id", $contact_ID);
+
+        return $query->execute();
+    }
+
+}
