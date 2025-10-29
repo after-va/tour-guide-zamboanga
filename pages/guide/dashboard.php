e73bb749d5cae28a8eedbac34ca2cfca114f8258
unknown <alelybitun.school@gmail.com>
2025-10-29 18:02:54 +0800

    add&update: booking system

--- pages/guide/dashboard.php
+++ pages/guide/dashboard.php
@@ -1,3 +1,12 @@
+<?php 
+session_start();
+if (!isset($_SESSION['user_id']) || $_SESSION['role_id'] != 2) {
+    header("Location: index.php");
+    exit();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
