41d65e12e2e6174b6f881aa98280f96f56f35f67
unknown <alelybitun.school@gmail.com>
2025-11-04 22:15:05 +0800

    feat: add navigation-bar to index fully collapsing

--- index.php
+++ index.php
@@ -6,21 +6,96 @@
     <title>Tourismo Zamboanga</title>
     
     <!-- Bootstrap File -->
-    <link rel="stylesheet" href="assets/css/bootstrap-grind.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-grid.css">
     <link rel="stylesheet" href="assets/css/bootstrap-reboot.css">
     <link rel="stylesheet" href="assets/css/bootstrap.css">
 
+    <!-- Vendor CSS Files
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet"> -->
 
-
-    
+    <!--Inner CSS Files -->
     <link rel="stylesheet" href="assets/css/index.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/header.css">
+    
+
+    <!-- ✅ Bootstrap 5.3.3 (CSS) -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 <body>
+    <header class = "header">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <div class="container">
+            <a class="navbar-brand fw-bold" href="#">Tourismo Zamboanga</a>
 
-    
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="mainNav">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Tour Packages</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Tour Spots</a></li>
+            </ul>
+            <a href="login.php" class="btn btn-info ms-lg-3">Sign In</a>
+            </div>
+        </div>
+        </nav>
+
+    </header>
+    <main>
+        <section id = "hero">
+            
+        </section>
+
+        <section id = "about">
+
+        </section>
+
+        <section id = "tour-spots">
+
+        </section>
+
+        <section id = "marketing">
+
+        </section>
+
+        <section id = "category">
+
+        </section>
+
+        <section id = "tourpackages">
+
+        </section>
+
+    </main>
+
+
+    <!-- Scripts -->
+    <script>
+        
+
+    </script>
+
+
+    <!-- Vendor JS Files
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/imagesloaded/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script> -->
+
+    <!-- ✅ Bootstrap JS (includes Popper.js for dropdowns/collapse) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
