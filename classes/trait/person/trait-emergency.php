e58da5a6cb667879c74a7fcbe9d9567f0c3f710c
unknown <alelybitun.school@gmail.com>
2025-11-15 12:24:35 +0800

    feat: added updateEmergencyID() in trait-emergency.php

--- classes/trait/person/trait-emergency.php
+++ classes/trait/person/trait-emergency.php
@@ -62,5 +62,48 @@ trait EmergencyTrait {
         }
     }
 
+    public function updateEmergencyID($emergency_ID, $country_ID, $phone_number, $ename, $erelationship, $db){
+        try {
+            $sql_count = "SELECT COUNT(DISTINCT emergency_ID) AS emergencyID_count
+                FROM Contact_Info WHERE emergency_ID = :emergency_ID ";
+
+            $q_count = $db->prepare($sql_count);
+            $q_count->execute([':name_ID' => $name_ID]);
+            $emergencyID_count = (int) $q_count->fetchColumn();
+
+            if ($emergencyID_count > 1) {
+                echo "Emergency ID {$emergency_ID} is shared by {$emergencyID_count} contact. Creating new name_ID for this person.\n";
+                $emergency_ID = $this->addgetEmergencyID($country_ID, $phone_number, $ename, $erelationship, $db);
+
+            } else {
+                echo "Reusing existing Emergency ID: {$emergency_ID} (Linked to {$emergencyID_count} person).\n";
+                
+                $phone_ID = $this->updatePhoneNumber($phone_ID, $country_ID, $phone_number, $db);
+
+                $sql_insert = "UPDATE emergency_info SET
+                    emergency_Name = :emergency_Name,
+                    emergency_Relationship = :emergency_Relationship,
+                    phone_ID = :phone_ID,
+                    WHERE emergency_ID = :emergency_ID";
+                $q_insert = $db->prepare($sql_insert);
+                $q_insert->bindParam(":firstname", $name_first);
+                $q_insert->bindParam(":secondname", $name_second);
+                $q_insert->bindParam(":middlename", $name_middle);
+                $q_insert->bindParam(":lastname", $name_last);
+                $q_insert->bindParam(":suffix", $name_suffix);
+                $existing = $q_check->fetch(PDO::FETCH_ASSOC);
+                if ($q_insert->execute()) {
+                    return $db->lastInsertId();
+                } else {
+                    return false;
+                }
+                
+            }
+
+
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
 
 }
